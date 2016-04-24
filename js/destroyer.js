ab1602f3356a4452019eee4cde2913d3921843e6
Marian <mmenschig@googlemail.com>
2016-04-24 10:31:40 -0700

    small improvements made

--- js/destroyer.js
+++ js/destroyer.js
@@ -1,6 +1,19 @@
 function destroyer(arr) {
-  // Remove all the values
-    return arr;
+
+    var newArray = Array.from(arguments);
+    var target = newArray[0];
+    var filter = newArray.slice(1, newArray.length);
+    
+    console.log(target);
+    console.log(filter);
+
+    
+    // var resultArray = target.filter()
+    
+
+
+
 }
+
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
