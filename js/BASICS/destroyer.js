6de9f1b086f421ae7af8e619f3c9a3d73dd3e4e2
Marian Menschig <mmenschig@riministreet.com>
2016-04-26 11:52:04 -0700

    discarded obsolete arguments

--- js/BASICS/destroyer.js
+++ js/BASICS/destroyer.js
@@ -1,9 +1,18 @@
-function destroyer(arr, arr2, arr3, arr4) {
+/** 
+ * [destroyer description]
+ * @param  {array} arr input array to filter
+ * @return {array}     array excluding filter values
+ * @author  mmenschig
+ * @version  1.0
+ */
+function destroyer(arr) {
 
     var passedFilter = Array.prototype.slice.call(arguments, 1);
 
+    // x is the current element that
+    // Array.filter() is iterating over
     var newArray = arr.filter(function (x) {
-    if (passedFilter.indexOf(x) !== -1){
+    if (passedFilter.indexOf(x) !== -1) {
         return false;
     }
         return true;
@@ -12,4 +21,4 @@ function destroyer(arr, arr2, arr3, arr4) {
     return newArray;
 }
 
-destroyer([1, 2, 5, 3, 2, 1], 1, 2, 5);
+console.log(destroyer([1, 2, 5, 3, 2, 1], 1, 2));
