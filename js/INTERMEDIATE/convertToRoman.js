985cec626b409676293372aacd8dd44e4c85577e
Marian Menschig <mmenschig@riministreet.com>
2016-06-08 17:53:56 -0700

    numeral converter

---
+++ js/INTERMEDIATE/convertToRoman.js
@@ -0,0 +1,17 @@
+symbols = {
+	1: "I",
+	5: 	"V",
+	10: "X",
+	50: "L",
+	100: "C",
+	500: "D",
+	1000: "M",
+}
+
+function convertToRoman(num) {
+	return num;
+}
+
+
+
+console.log(convertToRoman(36));
