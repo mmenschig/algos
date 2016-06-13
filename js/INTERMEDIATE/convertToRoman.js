045cf251a18f058b0fa2fb540abc6387bed614e0
Marian <mmenschig@googlemail.com>
2016-06-12 22:45:05 -0700

    continued work on roman numeral converter

--- js/INTERMEDIATE/convertToRoman.js
+++ js/INTERMEDIATE/convertToRoman.js
@@ -1,17 +1,44 @@
-symbols = {
-	1: "I",
-	5: "V",
-	10: "X",
-	50: "L",
-	100: "C",
-	500: "D",
-	1000: "M",
-}
+var symbols = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M'
+};
+
+var order = [1000, 500, 100, 50, 10,
+             9, 8, 7, 6, 5, 4, 3, 2, 1];
+
 
 function convertToRoman(num) {
-	return num;
-}
 
+    var conversionArray = []; // Holds roman numerals
 
+    if (num === 0) {
+        return "nulla";
+    }
 
-console.log(convertToRoman(36));
+    for (var i = 0; i < order.length; i++) {
+
+        if (num / order[i] >= 1) {
+            conversionArray.push(symbols[order[i]]);
+            num -= order[i];
+            i = -1; // restarting loop
+        }
+    }
+
+    // return conversionArray
+
+    return conversionArray.join("");
+};
+
+console.log(convertToRoman(3999));
