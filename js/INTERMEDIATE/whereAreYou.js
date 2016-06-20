8fc20de9271ae9829b2b3939a5f3b04ae0ce9039
Marian <mmenschig@googlemail.com>
2016-06-19 17:54:09 -0700

    finished function

--- js/INTERMEDIATE/whereAreYou.js
+++ js/INTERMEDIATE/whereAreYou.js
@@ -1,12 +1,24 @@
-
 function whereAreYou(collection, source) {
-  // What's in a name?
-  var arr = [];
-  // Only change code below this line
+  // Storing the property names from source
+  var sourceKeys = Object.keys(source);
+
+  // We filter the collection array
+  return collection.filter(function (obj) {
+    for (var prop in sourceKeys) {
+      // If collection object does not have the same property as source property
+      // or the values of the properties are not equal we return false and move on
+      if (!obj.hasOwnProperty(sourceKeys[prop]) || obj[sourceKeys[prop]] !== source[sourceKeys[prop]]) {
+        return false;
+      }
+    }
+
+    // We return true if both our 
+    // property names and the 
+    // corresponding values match
+    return true;
+
+  });
   
-  
-  // Only change code above this line
-  return arr;
 }
 
-whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet", middle: "Pedro" }], {first: "Tybalt"});
