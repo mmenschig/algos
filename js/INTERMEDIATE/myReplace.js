3ca055eba6aa6cfa90674c804606245015f725ed
Marian <mmenschig@googlemail.com>
2016-06-20 20:25:06 -0700

    completed challenge

--- js/INTERMEDIATE/myReplace.js
+++ js/INTERMEDIATE/myReplace.js
@@ -1,6 +1,21 @@
 
 function myReplace(str, before, after) {
-  return str;
+
+  var str = str.split(" ");
+  var beforeIndex = str.indexOf(before);
+
+  // Capitalizing after argument if
+  // before argument is capitalized
+  if (before[0] === before[0].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  }
+
+  // Removing 1 item at before and
+  // replacing it with new after
+  str.splice(beforeIndex, 1, after);
+
+  // returning space joined string
+  return str.join(" ");
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
