915e3222e2cca4cf677e4bbcbf68f2311076c3f2
Marian <mmenschig@googlemail.com>
2016-06-20 22:55:40 -0700

    completed algorithm challenge

--- js/INTERMEDIATE/translatePigLatin.js
+++ js/INTERMEDIATE/translatePigLatin.js
@@ -1,6 +1,26 @@
 
 function translatePigLatin(str) {
-  return str;
+  
+  // Reg Ex for consonant or consonant clusters
+  var re = /^([bcdfghjklmnpqrstvwxyz]{1,})/;
+
+  // executing regular expression,
+  // returns true if object is created
+  if (re.exec(str) !== null) {
+    
+    // number of consonants at beginning
+    // of string
+    var len = re.exec(str)[0].length;
+
+    // We move the found consonsants to the 
+    // end of the original string and add 
+    // "ay" behind the result
+    return (str.substr(len) + str.substr(0, len) + "ay");
+
+  } 
+    
+  // If the string begins with a vowel
+  return (str + "way");
 }
 
-translatePigLatin("consonant");
+translatePigLatin("glove");
