141c03eb24362a7a31e9245599b99b587be2750d
Marian <mmenschig@googlemail.com>
2016-06-27 21:46:18 -0700

    finished algo

--- js/INTERMEDIATE/pairElement.js
+++ js/INTERMEDIATE/pairElement.js
@@ -1,6 +1,32 @@
+// pair definition
+var pairs = {
+    'A': 'AT',
+    'T': 'TA',
+    'C': 'CG',
+    'G': 'GC'
+};
+
 
 function pairElement(str) {
-  return str;
+
+  var result = [];
+
+
+  for (var i = 0; i < str.length; i++) {
+    result.push(pairs[str[i]].split(''));
+  }
+
+  for (char in str) {
+    result.push(pairs[str[char]].split(''))
+    console.log(pairs[str[char]].split(''));
+  }
+
+  return result;
+
+
+
+
 }
+
 
 pairElement("GCG");
