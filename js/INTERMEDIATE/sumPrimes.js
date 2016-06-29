52022529f029acde8ad8ca9f6b2a1e933fbfc8f0
Marian <mmenschig@googlemail.com>
2016-06-28 22:31:18 -0700

    completed function

--- js/INTERMEDIATE/sumPrimes.js
+++ js/INTERMEDIATE/sumPrimes.js
@@ -1,6 +1,28 @@
 
 function sumPrimes(num) {
-  return num;
+  
+    var sum = 0;
+
+    for (var i = 1; i <= num; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
+
+    return sum + 1;
 }
 
-sumPrimes(10);
+function isPrime(num) {
+
+    for (var x = 2; x <= Math.pow(num, 0.5) + 1; x++) {
+        if (num % x === 0) {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+console.log(sumPrimes(977));
