da4e42d56e670ba8e8d250aa0f2d9795712a21e4
Marian <mmenschig@googlemail.com>
2016-08-14 20:47:45 -0700

    updated convertHTML

--- js/INTERMEDIATE/convertHTML.js
+++ js/INTERMEDIATE/convertHTML.js
@@ -1,7 +1,19 @@
 
 function convertHTML(str) {
-  // &colon;&rpar;
-  return str;
+
+    //Note: This is a super hacky solution and I'm
+    //      totally not a fan. This requires a rewrite
+    //      using an object or a more elegant solution.
+
+    var newstring;
+
+    newstring = str.replace(/&/g, '&amp;')
+                   .replace(/</g, '&lt;')
+                   .replace(/>/g, '&gt;')
+                   .replace(/"/g, '&quot;')
+                   .replace(/'/g, "&apos;")
+
+    return newstring
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML("Dolce & Gabbana is < than great");
