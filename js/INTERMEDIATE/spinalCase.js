b1532f4a1cbce36e4a8f2352e2eed818a528c415
Marian <mmenschig@googlemail.com>
2016-08-16 23:06:23 -0700

    update spinalcase

---
+++ js/INTERMEDIATE/spinalCase.js
@@ -0,0 +1,9 @@
+
+function spinalCase(str) {
+    var regex = /\s+|_+/g;
+    
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+    return str.replace(regex, '-').toLowerCase();
+}
+
+spinalCase('This Is Spinal Tap and the Andy_griffith_show');
