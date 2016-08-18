8aa228f167a8cb0df9f88bc50b4bc2ef7d853cc7
Marian Menschig <mmenschig@riministreet.com>
2016-08-17 21:15:00 -0700

    update uniteunique

--- js/INTERMEDIATE/uniteUnique.js
+++ js/INTERMEDIATE/uniteUnique.js
@@ -1,19 +1,22 @@
 
 function uniteUnique(arr) {
 
+	// storing the unique values in here	
     var combined = [];
     
+    // iterating through the arguments (arrays)
     for (var i = 0; i < arguments.length; i++) {
         argArray = arguments[i];
 
+        // iterating through the array items
         for (var j = 0; j < argArray.length; j++) {
 
+        	// storing value if not in 'combined' yet
             if (combined.indexOf(argArray[j]) === -1) {
                 combined.push(argArray[j]);
             }
         }
     }
-
 
     return combined;
 }
