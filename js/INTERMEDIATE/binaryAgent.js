4cdff4cac050f0e2f684c0dbc5b32e1327c00809
Marian Menschig <mmenschig@riministreet.com>
2016-09-20 17:01:03 -0700

    completed binary Agent

--- js/INTERMEDIATE/binaryAgent.js
+++ js/INTERMEDIATE/binaryAgent.js
@@ -3,16 +3,13 @@ function binaryAgent(str) {
     
     // Used to store 
     var strings = str.split(" ");
-    var result;
-    console.log(strings);
+    var result = [];
 
-    //TODO: Turn all binary strings into decimal strings
-    //      and then return character forEach
+    strings.forEach(function(item) {
+        result.push(String.fromCharCode(parseInt(item, 2)));
+    })
 
-    console.log(parseInt(strings, 2));
-    console.log(String.fromCharCode(parseInt(strings, 2)))
-
-    return str;
+    return result.join(separator='')
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
