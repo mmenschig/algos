217c4222919fa4b66a87d9a66b27b46137f87297
Marian <mmenschig@googlemail.com>
2016-09-28 22:21:34 -0700

    completed algorithm

--- js/INTERMEDIATE/addTogether.js
+++ js/INTERMEDIATE/addTogether.js
@@ -1,6 +1,30 @@
 
 function addTogether() {
-  return false;
+
+  // Check if either argument is not a valid number:
+  for (var arg = 0; arg < arguments.length; arg++) {
+    if (typeof arguments[arg] !== 'number') {
+      return undefined
+    }
+  }
+
+  // Using closures
+  if (arguments.length === 2) {
+    return arguments[0] + arguments[1];
+  } else {
+
+    var x = arguments[0];
+    return function(y) {
+
+      if (typeof arguments[0] !== 'number') {
+        return undefined;
+      }
+
+      return x + y;
+    };
+  }
+
 }
 
-addTogether(2,3);
+console.log(addTogether(2,3));
+console.log(addTogether(2)(11));
