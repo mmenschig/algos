25b76899529f32497279b91ca81020744f1d7af3
Marian Menschig <mmenschig@riministreet.com>
2016-10-04 22:00:21 -0700

    began advanced section

--- js/ADVANCED/telephoneCheck.js
+++ js/ADVANCED/telephoneCheck.js
@@ -1,9 +1,18 @@
 
 function telephoneCheck(str) {
 
-	// Check out https://regex101.com/
+  // Check out https://regex101.com/
+  //^(1|1\s|\()?([0-9]{3})?(-|\s|\))?([0-9]{3})?(-|\s|\))?([0-9]{4})?
+  
+  // The following should return true
+  // 555-555-5555
+  // (555)555-5555
+  // (555) 555-5555
+  // 555 555 5555
+  // 5555555555
+  // 1 555 555 5555
 
-	return true;
+  return str.test(pattern);
 
 }
 
