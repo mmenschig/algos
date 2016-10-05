a4d6f5426f50dd9a9f2b4e081cdba0d2f86b6a74
Marian <mmenschig@googlemail.com>
2016-10-04 23:27:46 -0700

    completed

--- js/ADVANCED/telephoneCheck.js
+++ js/ADVANCED/telephoneCheck.js
@@ -1,21 +1,9 @@
 
 function telephoneCheck(str) {
 
-  // Check out https://regex101.com/
-  //^(1|1\s|\()?([0-9]{3})?(-|\s|\))?([0-9]{3})?(-|\s|\))?([0-9]{4})?
-  
-  // The following should return true
-  // 555-555-5555
-  // (555)555-5555
-  // (555) 555-5555
-  // 555 555 5555
-  // 5555555555
-  // 1 555 555 5555
-
-  return str.test(pattern);
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
 
 }
-
-
 
 telephoneCheck("555-555-5555");
